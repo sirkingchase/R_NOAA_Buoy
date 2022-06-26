0ad61adc01a98ff6d2df5587dd094d72602e7177
Chase <sirkingchase@gmail.com>
2022-06-26 17:11:29 -0500

    Converts columns to imperial, round to nearest tenth, remove incomplete data rows

--- NOAA_Buoys.r
+++ NOAA_Buoys.r
@@ -9,7 +9,10 @@ library(measurements)
 headers = read.table("42020h2021.txt", skip = 0, header = FALSE, nrows = 1, as.is = T, comment.char = "")
 x<-read.table("42020h2021.txt", header=FALSE)
 colnames(x)= headers
-names(x)[1] <- "YY" #remove that comment
+names(x)[1] <- "YY" #remove that prepended comment
+
+#remove incomplete data
+x <- filter(x, WVHT < 99 & DPD < 99)
 
 headerMeta = read.table("42020h2021.txt", skip = 1, header = FALSE, nrows = 1, as.is = T, comment.char = "")
 #https://www.unidata.ucar.edu/software/udunits/udunits-2.2.28/udunits2-common.xml
@@ -26,37 +29,46 @@ units = headerMeta
 #pair the meta data with the column headers
 data_frame = x %>% map2_dfc(units,  ~set_units(.x, .y, mode = "standard"))
 
+
+
+#by(data_sorted[!(WVHT %in% c(99))],
+
+
 #https://r-quantities.github.io/units/articles/measurement_units_in_R.html
 #https://www.rdocumentation.org/packages/units/versions/0.8-0/topics/units
 #lets do some converstion from metric to imperial
-units(data_frame$WVHT) <- make_units(ft)
 
-print(data_frame)
+units(data_frame$WVHT) <- make_units(ft)
+units(data_frame$WSPD) <- make_units(kts) #knots AKA mph
+units(data_frame$GST) <- make_units(kts)
+
+units(data_frame$ATMP) <- make_units(degrees_F)
+units(data_frame$WTMP) <- make_units(degrees_F)
+units(data_frame$DEWP) <- make_units(degrees_F)
+
+#rint(data_frame)
 
 
 
 #TODO: Analyze trends in dominate wave peroids & wave heights
 #TODO: Marry this data with over sources ex. historical weather reports like was it raining that day?
 # sorting the data by the column
-data_sorted <- x[order(x$WVHT, decreasing = TRUE), ]
+data_frame <- round(data_frame, digits=1) #round to nearest tenth ex. 1.1
+data_sorted <- data_frame[
+    order(
+      data_frame$WVHT, 
+      #data_frame$DPD,
+      decreasing = TRUE), 
+  ]
 
 # select top 3 values from each group
 data_mod <- Reduce(rbind,                               
                    by(data_sorted,
                       data_sorted["DPD"],
+                      #data_sorted["WVHT"],
                       head,
                       n = 3))
 
-#https://r-graphics.org/recipe-dataprep-calculate#:~:text=With%20base%20R%2C%20calculating%20a%20new%20colum%20can,heightweight%24heightCm%20%3C-%20heightweight%24heightIn%20%2A%202.54%2015.15.4%20See%20Also
-# print(data_mod %>%
-#         mutate(
-#           wave_height_feet =  conv_unit(data_mod["WVHT"], "m", "ft"),
-#           water_temp =  conv_unit(data_mod["WTMP"], "C", "F"),
-#           air_temp =  conv_unit(data_mod["ATMP"], "C", "F")
-#         )
-# )
 
-#print ("Modified DataFrame")
-#print (data_mod)
-
-write.csv(data_frame, "dominateWavePeroid.csv", row.names=TRUE)
+print (data_mod)
+write.csv(data_mod, "dominateWavePeroid.csv", row.names=TRUE)
